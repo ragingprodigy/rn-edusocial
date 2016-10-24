372c52c571cf56c37fa8793b03c6dabb278d3f98
Omonayajo Oladapo <o.omonayajo@gmail.com>
2016-10-24 10:24:19 +0100

    Show a student's list of courses

---
+++ app/components/courses/courseSchema.js
@@ -0,0 +1,14 @@
+/**
+ * Created by oladapo on 24/10/2016.
+ * @flow
+ */
+'use strict';
+export type Course = {
+  id: string,
+  "_id": number,
+  course_code: string,
+  course_title: string,
+  course_description: string,
+  hashcode: string,
+  "__v": number
+};
